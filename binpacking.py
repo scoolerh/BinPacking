805bb52b0d79cdd2099531cef3a749387b380790
Bryce <bryce.h.bernstein@gmail.com>
2023-09-13 13:27:44 -0500

    Fixed bug and cleaned up code (a little)

--- binpacking.py
+++ binpacking.py
@@ -19,31 +19,34 @@ def newproblem():
 
 @app.route('/placeitem/<problemID>/<size>')
 def placeitem(problemID, size):
-    problem = binsList[int(problemID)]
+    problemID = int(problemID)
     size = int(size)
-    bins = problem[2:-1].split('#')
+    problem = binsList[problemID]
+    if size > 100 :
+        return "Failed to add to bin. Item is sized greater than 100."
+    bins = problem[2:-2].split('#')
     added = False
     for i in range(len(bins)):
         sizeList = bins[i].split('!')
-        if (sizeList[0] == ''): 
-            bins[i] = str(size)
+        if sizeList[0] == '':
+            bins[i] = size
             added = True
             break
         spaceTaken = 0
         for item in sizeList: 
             spaceTaken += int(item)
-        if (spaceTaken + size <= 100): 
+        if spaceTaken + size <= 100:
             bins[i] += '!' + str(size)
             added = True
             break
-    if (added == False): 
-        bins.append(str(size))
+    if not added:
+        bins.append(size)
     final = '##'
     for bin in bins: 
-        final += bin + '#'
-    #final += '' if len(bins) == 0 else '#'
-    binsList[int(problemID)] = final
-    return final + "#"
+        final += str(bin) + '#'
+    final += '' if len(bins) == 0 else '#'
+    binsList[problemID] = final
+    return final
 
 if __name__ == '__main__': 
     host = 'localhost'
